b4833babc00f2d13fe4c908f62236220c6ce6ada
Mende Bozhinovski <mende.bozhinovski@endava.com>
2025-11-20 12:04:07 +0100

    push

--- vpc.tf
+++ vpc.tf
@@ -1,13 +1,13 @@
-resource "aws_vpc" "default" {
-  cidr_block           = "172.31.0.0/16"
-  enable_dns_hostnames = true
-  enable_dns_support   = true
-  instance_tenancy     = "default"
+# resource "aws_vpc" "default" {
+#   cidr_block           = "172.31.0.0/16"
+#   enable_dns_hostnames = true
+#   enable_dns_support   = true
+#   instance_tenancy     = "default"
 
-  tags = {
-    "Name" = "default"
-  }
-}
+#   tags = {
+#     "Name" = "default"
+#   }
+# }
 
 resource "aws_vpc" "prod_eks" {
   cidr_block           = "10.0.0.0/16"
