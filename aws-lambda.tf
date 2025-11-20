9eb4ac07af55c5f3cd1ccc856188430d1b8bb698
Mende Bozhinovski <mende.bozhinovski@endava.com>
2025-11-20 11:58:18 +0100

    add s3 backend

--- aws-lambda.tf
+++ aws-lambda.tf
@@ -1,7 +1,7 @@
-resource "aws_lambda_permission" "allow_bucket_prod_ftp_backup" {
-  statement_id  = "AllowExecutionFromS3BucketProdFTPBackup"
-  action        = "lambda:InvokeFunction"
-  function_name = "arn:aws:lambda:${var.aws_region}:${var.account_id}:function:run-jenkins-job-ftp-backup-restore"
-  principal     = "s3.amazonaws.com"
-  source_arn    = aws_s3_bucket.prod-ftp-backup-mende.arn
-}
+# resource "aws_lambda_permission" "allow_bucket_prod_ftp_backup" {
+#   statement_id  = "AllowExecutionFromS3BucketProdFTPBackup"
+#   action        = "lambda:InvokeFunction"
+#   function_name = "arn:aws:lambda:${var.aws_region}:${var.account_id}:function:run-jenkins-job-ftp-backup-restore"
+#   principal     = "s3.amazonaws.com"
+#   source_arn    = aws_s3_bucket.prod-ftp-backup-mende.arn
+# }
